f655d923ba49d2d5ad3bc7c9347cebc439fedb9a
R-Katoch <77954196+R-Katoch@users.noreply.github.com>
2026-05-20 22:53:22 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,5 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 rl.on("line", (line) => {
-  rl.prompt();
   console.log(`${line}: command not found`);
 })
