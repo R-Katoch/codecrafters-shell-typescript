e4cfc4d712fc41e7b222f015624cacc261e17326
R-Katoch <77954196+R-Katoch@users.noreply.github.com>
2026-05-24 00:05:20 +0530

    codecrafters submit [skip ci]

--- app/helper/parser/parser.ts
+++ app/helper/parser/parser.ts
@@ -57,6 +57,16 @@ export function parse(input: string): ParsedCommand {
       continue;
     }
 
+    if (token === "1>") {
+      redirects.push({
+        stream: "stdout",
+        mode: "write",
+        target: tokens[++i],
+      });
+
+      continue;
+    }
+
     args.push(token);
   }
 
