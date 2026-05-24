51b3199bfc3b0f592991e09413add14399846ff1
R-Katoch <77954196+R-Katoch@users.noreply.github.com>
2026-05-24 20:08:14 +0530

    codecrafters submit [skip ci]

--- app/commands/builtins/complete/complete.ts
+++ app/commands/builtins/complete/complete.ts
@@ -1,6 +1,13 @@
 import type { Command, CommandContext } from "../../../types";
 
 export class CompleteCommand implements Command {
-    readonly name = "complete";
-    execute(context: CommandContext): void {}
+  readonly name = "complete";
+
+  execute(context: CommandContext): void {
+    const { args, stdout } = context;
+
+    if (args[0] === "-p") {
+      stdout.write(`${this.name}: ${args[1]}: no completion specification\n`);
+    }
+  }
 }
